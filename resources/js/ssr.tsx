73ce71145ed533e29fed49d2d4a906485438595c
matteoiscrying <67237370+matteoiscrying@users.noreply.github.com>
2024-10-24 18:31:48 +1100

    SSR

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -19,12 +19,9 @@ createServer((page) =>
             ),
         setup: ({ App, props }) => {
             /* eslint-disable */
-            // @ts-expect-error
             global.route<RouteName> = (name, params, absolute) =>
                 route(name, params as any, absolute, {
-                    // @ts-expect-error
                     ...page.props.ziggy,
-                    // @ts-expect-error
                     location: new URL(page.props.ziggy.location),
                 });
             /* eslint-enable */
